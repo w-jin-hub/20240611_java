5c8aa1c99fe95db69d9a318c10534803a9ed276d
w-jin-hub <wjsdnwls604@gmail.com>
2024-06-11 15:20:29 +0900

    init

--- src/p01_class/AnimalEx.java
+++ src/p01_class/AnimalEx.java
@@ -1,9 +1,25 @@
 package p01_class;
 
+import java.sql.SQLOutput;
+
 public class AnimalEx {
 
+  Animal a2 = new Animal();
+
+  // Quiz) animal을 인스턴스로 만들어 사용해 보시요
   public void done() {
     Animal a1 = new Animal();
     a1.run();
+    a2.run();
   }
+
+  public static void main(String[] args) {
+    Animal a1 = new Animal();
+    a1.run();
+//    a2.run(); // static은 같은 static은 끼리만 사용 시점이 다르다.
+    AnimalEx animalEx = new AnimalEx();
+    animalEx.done();
+    System.out.println(animalEx.a2);
+  }
+
 }
---
+++ src/p02/varialbe/Ex01.java
@@ -0,0 +1,10 @@
+package p02.varialbe;
+// 변수
+public class Ex01 {
+  // 1. 변수의 선언 위치 : 전역, 지역
+  int sum; // 전역변수
+  public void sum() {
+    int sum; // 지역변수
+  }
+
+}
