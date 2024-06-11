32daf156ed9a4049995430efaf0c7e49fdfe4741
w-jin-hub <wjsdnwls604@gmail.com>
2024-06-11 15:35:47 +0900

    init

--- src/p02/varialbe/Ex01.java
+++ src/p02/varialbe/Ex01.java
@@ -2,9 +2,22 @@ package p02.varialbe;
 // 변수
 public class Ex01 {
   // 1. 변수의 선언 위치 : 전역, 지역
-  int sum; // 전역변수
+  static int sum = 0; // 멤버변스는 전역변수(global variable)
+  // int sum = 1; // 중복 선언되어서 사용 할 수 없다.
+
   public void sum() {
-    int sum; // 지역변수
+    int sum = 1; // 지역변수(local variable)
+//    System.out.println(sum);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(sum);
+
+    Ex01 ex01 = new Ex01();
+    System.out.println(ex01.sum);
+
+    ex01.sum();
+
   }
 
 }
