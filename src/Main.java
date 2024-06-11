9c615a6a72ea6aa8bc8ebdb0ad457948082a7b12
w-jin-hub <wjsdnwls604@gmail.com>
2024-06-11 14:24:01 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import p01_class.Animal;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+//    Main main = new Main();
+//    main.done();
+    Animal animal = new Animal();
+    animal.legs = 4;
+    Animal animal2 = new Animal();
+    animal2.legs = 2;
+    System.out.println(animal.legs);
+    System.out.println(animal2.legs);
+    animal.run();
+    animal2.run();
   }
 }
