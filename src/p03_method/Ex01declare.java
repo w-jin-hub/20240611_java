d58d7c217c43277e7b5cb7061360ff6efd47c384
w-jin-hub <wjsdnwls604@gmail.com>
2024-06-17 16:01:27 +0900

    init

--- src/p03_method/Ex01declare.java
+++ src/p03_method/Ex01declare.java
@@ -59,6 +59,7 @@ public class Ex01declare {
     System.out.println(MyMath.divied(10, 3));
     System.out.println(Math.max(1,3));
     System.out.println(MyMath.share(10, 3));
+    MyMath.shareRemain(10, 3);
   }
 }
 
@@ -89,8 +90,12 @@ class MyMath {
     return n1 % n2;
   }
   public static void shareRemain(int n1 ,int n2) {
-    System.out.println("몫" + share(n1, n2));
-    System.out.println("나머지" + remain(n1, n2));
+    //System.out.println("몫" + share(n1, n2));
+    //System.out.println("나머지" + remain(n1, n2));
+    int a = share(n1, n2);
+    int b = remain(n1, n2);
+    System.out.printf("몫 : %d, 나머지 : %d", a, b);
+    System.out.println(String.format("몫 : %d, 나머지 : %d", share(n1, n2), remain(n1, n2)));
   }
 
   public static void main(String[] args) {
